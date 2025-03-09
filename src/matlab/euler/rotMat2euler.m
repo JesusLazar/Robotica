d2607e0ac7101267d4e5528e576109ee7f45e8d4
Lazaro <jss.lzr.07@gmail.com>
2025-03-08 17:09:20 -0700

    Ejercicio de euler

--- src/matlab/euler/rotMat2euler.m
+++ src/matlab/euler/rotMat2euler.m
@@ -10,9 +10,10 @@ function euler = rotMat2euler(R, secuencia)
 
 % Obtén las ecuaciones de la imagen
 if secuencia == "XYZ"
-    phi =       % phi:   rotación alrededor del eje X
-    theta =     % theta: rotación alrededor del eje Y
-    psi =       % psi:   rotación alrededor del eje Z
-    euler =
+ phi = atan2(-R(2, 3), R(3, 3));   
+ theta = asin(R(1, 3));           
+ psi = atan2(-R(1, 2), R(1, 1));  
+
+ euler = rad2deg([phi; theta; psi]);
 end
 
